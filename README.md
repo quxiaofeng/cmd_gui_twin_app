5bbd3158045a7c8deb73b5f05f0886d5c0f0f3df
Qu Xiaofeng <quxiaofeng.at.polyu@gmail.com>
2012-10-16 10:41:59 +0800

    revise test
    Signed-off-by: Qu Xiaofeng <quxiaofeng.at.polyu@gmail.com>

--- README.md
+++ README.md
@@ -7,6 +7,11 @@ A simple demo on a cmd and gui twin app with the same function
 
 This application is based on Qt 4.8.1 and OpenCV 2.4.2 on msvc2010 32bit platform.
 
+Need 
+
+```batch
+set PATH = path/to/QtSDK/mscv2010/;$PATH$ 
+``````
 ## Structure
 
 ### algorithms
--- test/roc_cmd_test.bat
+++ test/roc_cmd_test.bat
@@ -1,15 +1,23 @@
+if not exist ..\roc_cmd-build-desktop-Qt_4_8_1_for_Desktop_-_MSVC2010__Qt_SDK____\release\roc_cmd.exe goto NoReleaseCmd
 start ..\roc_cmd-build-desktop-Qt_4_8_1_for_Desktop_-_MSVC2010__Qt_SDK____\release\roc_cmd.exe test1.png result\result1.bmp
 start ..\roc_cmd-build-desktop-Qt_4_8_1_for_Desktop_-_MSVC2010__Qt_SDK____\release\roc_cmd.exe test1.png result1.bmp
 start ..\roc_cmd-build-desktop-Qt_4_8_1_for_Desktop_-_MSVC2010__Qt_SDK____\release\roc_cmd.exe test1.png
 start ..\roc_cmd-build-desktop-Qt_4_8_1_for_Desktop_-_MSVC2010__Qt_SDK____\release\roc_cmd.exe test1.png result1.bmp result1.bmp2
 start ..\roc_cmd-build-desktop-Qt_4_8_1_for_Desktop_-_MSVC2010__Qt_SDK____\release\roc_cmd.exe test1.png
+:NoReleaseCmd
 
+if not exist ..\roc_cmd-build-desktop-Qt_4_8_1_for_Desktop_-_MSVC2010__Qt_SDK____\debug\roc_cmd.exe goto NoDebugCmd
 start ..\roc_cmd-build-desktop-Qt_4_8_1_for_Desktop_-_MSVC2010__Qt_SDK____\debug\roc_cmd.exe test2.png result\result2.bmp
 start ..\roc_cmd-build-desktop-Qt_4_8_1_for_Desktop_-_MSVC2010__Qt_SDK____\debug\roc_cmd.exe test2.png result2.bmp
 start ..\roc_cmd-build-desktop-Qt_4_8_1_for_Desktop_-_MSVC2010__Qt_SDK____\debug\roc_cmd.exe test2.png
 start ..\roc_cmd-build-desktop-Qt_4_8_1_for_Desktop_-_MSVC2010__Qt_SDK____\debug\roc_cmd.exe test2.png result2.bmp result2.bmp2
 start ..\roc_cmd-build-desktop-Qt_4_8_1_for_Desktop_-_MSVC2010__Qt_SDK____\debug\roc_cmd.exe test2.png
+:NoDebugCmd
 
+if not exist ..\roc_gui-build-desktop-Qt_4_8_1_for_Desktop_-_MSVC2010__Qt_SDK____\release\roc_gui.exe goto NoReleaseGui
 start ..\roc_gui-build-desktop-Qt_4_8_1_for_Desktop_-_MSVC2010__Qt_SDK____\release\roc_gui.exe test1.png
+:NoReleaseGui
 
+if not exist ..\roc_gui-build-desktop-Qt_4_8_1_for_Desktop_-_MSVC2010__Qt_SDK____\debug\roc_gui.exe goto NoDebugGui
 start ..\roc_gui-build-desktop-Qt_4_8_1_for_Desktop_-_MSVC2010__Qt_SDK____\debug\roc_gui.exe test1.png
+:NoDebugGui
